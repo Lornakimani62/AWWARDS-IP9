dbf1b9652f27fcb4c5008d3baf721c5be39ce80c
lorna <kimanilorna@yahoo.com>
2019-01-06 19:24:31 +0300

    Adds tests for the project class and its methods

--- awwwards/models.py
+++ awwwards/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
+from url_or_relative_url_field.fields import URLOrRelativeURLField
 
-# Create your models here.
+
 class Profile(models.Model):
+    '''A class to allow users to update and view their profile
+        '''
     avatar = models.ImageField(upload_to='avatars/')
     description = HTMLField()
     username = models.ForeignKey(User,on_delete=models.CASCADE)
     email = models.EmailField()
+    project = models.ForeignKey('Project',on_delete=models.CASCADE,null=True)
 
     def save_profile(self):
         self.save()
@@ -18,4 +22,27 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+
+class Project(models.Model):
+    '''A class that allows users to post,view and rate projects
+        '''
     
+    title = models.CharField(max_length=20)
+    description = HTMLField()
+    image = models.ImageField(upload_to='avatars/')
+    link = URLOrRelativeURLField()
+    username =  username = models.ForeignKey(User,on_delete=models.CASCADE)
+    design = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    usability = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    content = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    vote_submissions = models.IntegerField(default=0)
+
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    def __str__(self):
+        return self.project_title
--- awwwards/tests.py
+++ awwwards/tests.py
@@ -10,7 +10,7 @@ class ProfileTestClass(TestCase):
     def setUp(self):
         #initial instance to run at the begginning of every test
         self.username = User.objects.create_user('lorna','pas123')
-        self.new_profile = Profile(description='Test',avatar='',username=self.username,email='kimanilorna@yahoo.com' )
+        self.new_profile = Profile(description='Test',avatar='',username=self.username,email='kimanilorna@yahoo.com')
     
     def tearDown(self):
         """ Runs after each profile test case
@@ -38,3 +38,23 @@ class ProfileTestClass(TestCase):
         self.new_profile.delete_profile()
         profiles = Profile.objects.all()
         self.assertTrue(len(profiles) == 0)
+
+class ProjectTestClass(TestCase):
+    def setUp(self):
+        self.new_user = User.objects.create_user(username='user',password='user-password')
+        self.new_profile = Profile(username=self.new_user)
+        self.new_profile.save()
+        self.new_project = Project(id=1,image='photos/photo',title='Test Project',description='Test Description',username=self.new_user,link='http://livesite.com')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
+
+    def test_save_project(self):
+        self.new_project.save_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) > 0)
+
+    def test_delete_project(self):
+        self.new_project.delete_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) == 0)
