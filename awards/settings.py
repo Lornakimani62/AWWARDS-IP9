4cfe02c1487806ff7d5d11591db6ccc1ecfaec3c
lorna <kimanilorna@yahoo.com>
2019-01-07 06:07:05 +0300

    Adds the restful API feature for both project and profile classes

--- awards/settings.py
+++ awards/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'awwwards',
     'bootstrap4',
-    'registration',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
